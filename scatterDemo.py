226d8c6f5815597bd99ecf74742d510ae17eaab4
zhengxiang <1209549574@qq.com>
2018-12-04 20:38:12 +0800

    examples of scatter

--- scatterDemo.py
+++ scatterDemo.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# the examples of scatter
 import seaborn as sns
 import matplotlib.pyplot as plt
 
@@ -28,7 +29,17 @@ print(tips)
 # In the examples above, the hue semantic was categorical, so the default qualitative palette was applied.
 # If the hue semantic is numeric (specifically, if it can be cast to float), the default coloring switches
 # to a sequential palette
-sns.relplot(x="total_bill", y="tip", hue="size", data=tips)
+# sns.relplot(x="total_bill", y="tip", hue="size", data=tips)
+
+# you can also customize the color palette
+# sns.relplot(x="total_bill", y="tip", hue="size", palette="ch:r=-.5,l=.75", data=tips)
+
+# you can also customize the size of each point
+# sns.relplot(x="total_bill", y="tip", size="size", data=tips)
+
+# Unlike with matplotlib.pyplot.scatter(), the literal value of the variable is not used to pick the area of the point.
+# Instead, the range of values in data units is normalized into a range in area units. This range can be customized
+sns.relplot(x="total_bill", y="tip", size="size", sizes=(15, 200), data=tips)
 
 plt.show()
 
